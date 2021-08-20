95b42b7dccd8fbd7072ff0951c64fbfe209e84ea
eltos <eltos@outlook.de>
2021-08-20 18:11:01 +0200

    Add SVG and GIMP formats

--- generators.js
+++ generators.js
@@ -13,6 +13,8 @@ codeFlavours = {
     'code-css':            ['CSS',             gradient_as_css,             'language-css'       ],
     'code-matplotlib':     ['Matplotlib',      gradient_as_matplotlib,      'language-python'    ],
     'code-fastled':        ['FastLED',         gradient_as_fastled,         'language-ino'       ],
+    'code-svg':            ['SVG',             gradient_as_svg,             'language-xml'       ],
+    'code-gimp':           ['GIMP',            gradient_as_gimp,            undefined            ],
 }
 
 
@@ -51,6 +53,36 @@ function gradient_as_matplotlib(gradient, name, comment) {
             x => "(" + x.pos.toFixed(3) + ", "
                 + "(" + x.rgb.map(v => (v / 255).toFixed(3)).join(", ") + "))"
         ).join(",\n    ") + "))";
+}
+
+
+function gradient_as_svg(gradient, name, comment){
+    return "<linearGradient id=\"" + name + "\">\n" +
+        (comment ? "    <!-- " + comment + " -->\n" : "") +
+        "    " + gradient.map(
+            x => "<stop offset=\"" + x.pos.toFixed(3) +
+                "\" stop-color=\"#" + x.colorHex + "\" />"
+        ).join("\n    ") + "\n" +
+        "</linearGradient>";
+}
+
+
+function gradient_as_gimp(gradient, name, comment) {
+    let raw = "GIMP Gradient\n" +
+        "Name: " + name + "\n" +
+        (gradient.length - 1) + "\n";
+    for (let i = 0; i < gradient.length-1; i++) {
+        let l = gradient[i], r = gradient[i+1];
+        raw += [l.pos, (l.pos+r.pos)/2, r.pos, // position left, mid, right
+            l.r/255, l.g/255, l.b/255, 1,      // left color R, G, B, A
+            r.r/255, r.g/255, r.b/255, 1       // right color R, G, B, A
+        ].map(x => x.toFixed(3)).join(" ");
+        raw += " 0 0\n";  // blend type: linear, color type: RGB
+    }
+    if (comment) {
+        raw += "# " + comment;
+    }
+    return raw;
 }
 
 
